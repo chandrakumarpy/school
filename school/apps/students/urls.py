93395658110342b48b773e91d9cdb49141ffbd29
chandrakumarpy <pchandrakumarpy@gmail.com>
2022-02-13 22:11:10 +0530

    changes over

--- school/apps/students/urls.py
+++ school/apps/students/urls.py
@@ -19,3 +19,13 @@ urlpatterns = [
     path("upload/", StudentBulkUploadView.as_view(), name="student-upload"),
     path("download-csv/", DownloadCSVViewdownloadcsv.as_view(), name="download-csv"),
 ]
+
+# urlpatterns = [
+#     path("list", StudentListView.as_view(), name="student-list"),
+#     path("<int:pk>/", StudentDetailView.as_view(), name="student-detail"),
+#     path("create/", StudentCreateView.as_view(), name="student-create"),
+#     path("<int:pk>/update/", StudentUpdateView.as_view(), name="student-update"),
+#     path("delete/<int:pk>/", StudentDeleteView.as_view(), name="student-delete"),
+#     path("upload/", StudentBulkUploadView.as_view(), name="student-upload"),
+#     path("download-csv/", DownloadCSVViewdownloadcsv.as_view(), name="download-csv"),
+# ]
